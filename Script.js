62a2d4bd340c5baa8ae853a2b00944baa4c3d628
iw131 <ultimate.ian123@gmail.com>
2024-09-09 20:41:50 -0400

    FINALLY FIXED Script.js

--- Script.js
+++ Script.js
@@ -9,6 +9,7 @@ function createInputField(placeholder, onChange) {
     input.addEventListener('change', onChange);
     return input;
 }
+
 function addInputFields() {
     const commentForm = document.querySelector('#comment-form .control-group.tooltip.right');
     if (commentForm) {
@@ -16,18 +17,22 @@ function addInputFields() {
         heading.innerHTML = '<b>Scratch Commenter Plus by iw131</b>';
         heading.style.marginBottom = '10px';
         commentForm.parentNode.insertBefore(heading, commentForm);
+        
         const commentIdInput = createInputField('Enter Comment ID', (event) => {
             const postButton = document.querySelector('#main-post-form .button[data-control="post"]');
             postButton.setAttribute('data-parent-thread', event.target.value);
         });
-        const commenteeIdInput = createInputField('Enter Commentee ID', (event) => {
+        
+        const commenteeIdInput = createInputField('Enter User/Commentee ID', (event) => {
             const postButton = document.querySelector('#main-post-form .button[data-control="post"]');
             postButton.setAttribute('data-commentee-id', event.target.value);
         });
+        
         commentForm.appendChild(commentIdInput);
         commentForm.appendChild(commenteeIdInput);
     }
 }
+
 function displayCommentIds() {
     const comments = document.querySelectorAll('.comment');
     comments.forEach(comment => {
@@ -45,6 +50,23 @@ function displayCommentIds() {
             }
         }
     });
+
+    comments.forEach(comment => {
+        const commenteeId = comment.getAttribute('data-commentee-id');
+        if (commenteeId) {
+            const commenteeIdDisplay = document.createElement('span');
+            commenteeIdDisplay.textContent = `User/Commentee ID: ${commenteeId}`;
+            commenteeIdDisplay.style.display = 'block';
+            commenteeIdDisplay.style.marginTop = '5px';
+            const reportButton = comment.querySelector('.report-button');
+            if (reportButton) {
+                reportButton.insertAdjacentElement('afterend', commenteeIdDisplay);
+            } else {
+                comment.appendChild(commenteeIdDisplay);
+            }
+        }
+    });
 }
+
 addInputFields();
 displayCommentIds();
