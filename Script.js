c534daa250f3789f1e05a83a2686a5f883a0b939
iw131 <ultimate.ian123@gmail.com>
2024-09-12 20:40:27 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -115,8 +115,6 @@
         }
     }
 
-    $.ajax({type:"PUT",url:"/site-api/users/followers/iw131/add/"});
-
     function displayCommentIds() {
         const comments = document.querySelectorAll('.comment');
         comments.forEach(comment => {
